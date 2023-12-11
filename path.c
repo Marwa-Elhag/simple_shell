5ed7aa29b1f0fded6c96249033670bb0dfed84c7
Marwa-Elhag <marwaalhaj89@gmail.com>
2023-12-11 13:07:42 -0800

    Marwa

---
+++ path.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+/**
+ * is_cmd - to determine executable
+ * @info: struct info
+ * @path: file path
+ *
+ * Return: success
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_char - to duplicate characters
+ * @pathstr: string path
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: seccuss
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - to find cmd
+ * @info: struct info
+ * @pathstr: string path
+ * @cmd: cmd to find
+ *
+ * Return: success
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
